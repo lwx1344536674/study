a0459f49eb7e79bef0bc9a5b10ecdfe470344a45
1344536674@qq.com <1344536674@qq.com>
2020-04-23 17:18:03 +0800

    更换字体图标

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,16 +1,94 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+	<el-container class="home-container">
+		<!-- 头部区域 -->
+		<el-header>
+			<div class="header-img">
+				<!-- <img src="../../assets/logo.png" alt="" /> -->
+				<span>后台管理系统</span>
+			</div>
+			<el-button type="info" @click="logout">退出</el-button>
+		</el-header>
+		<!-- 页面主体区域 -->
+		<el-container>
+			<!-- 侧栏区域 -->
+			<el-aside width="200px">
+				<!-- 侧边栏菜单区域 -->
+				<el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff">
+					<!-- 一级菜单 -->
+					<el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+						<!-- 一级菜单模板区域 -->
+						<template slot="title">
+							<!-- 图标 -->
+							<i class="el-icon-menu"></i>
+							<!-- 文本 -->
+							<span>{{ item.authName }}</span>
+						</template>
+						<!-- 二级菜单 -->
+						<el-menu-item
+							:index="subitem.id + ''"
+							v-for="subitem in item.children"
+							:key="subitem.id"
+						>
+							<template slot="title">
+								<!-- 图标 -->
+								<i class="el-icon-menu"></i>
+								<!-- 文本 -->
+								<span>{{ subitem.authName }}</span>
+							</template>
+						</el-menu-item>
+					</el-submenu>
+				</el-menu>
+			</el-aside>
+			<!-- 右主体 -->
+			<el-main>Main</el-main>
+		</el-container>
+	</el-container>
 </template>
 <script>
 export default {
-  methods: {
-    logout() {
-      window.sessionStorage.clear()
-      this.$router.push('/login')
-    },
-  },
+	data() {
+		return {
+			menuList: [],
+		}
+	},
+	methods: {
+		logout() {
+			window.sessionStorage.clear()
+			this.$router.push('/login')
+		},
+		// 获取菜单列表
+		async getMenuList() {
+			const { data: res } = await this.$http.get('menus')
+			if (res.meta.status !== 200) return this.$message.errror(res.message)
+			this.menuList = res.data
+			console.log(res)
+		},
+	},
+	created() {
+		this.getMenuList()
+	},
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-header {
+	background-color: #373d41;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	color: #fff;
+	font-size: 20px;
+	div {
+		display: flex;
+		align-items: center;
+	}
+}
+.el-aside {
+	background-color: #333744;
+}
+.el-main {
+	background-color: #eaedf1;
+}
+.home-container {
+	height: 100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,28 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router/route';
-import ElementUI, { Message } from 'element-ui';
-import axios from 'axios';
-import 'element-ui/lib/theme-chalk/index.css';
-import './assets/font_icon/iconfont.css';
-import store from './store';
-axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/';
-Vue.prototype.$http = axios;
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/route'
+import ElementUI, { Message } from 'element-ui'
+import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/font_icon/iconfont.css'
+import store from './store'
 
-Vue.use(ElementUI);
-Vue.prototype.$message = Message;
+Vue.prototype.$http = axios
+
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+
+axios.interceptors.request.use((config) => {
+  console.log(config)
+
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+Vue.use(ElementUI)
+Vue.prototype.$message = Message
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount('#app')
